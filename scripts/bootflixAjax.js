2f71c187a1149dc9b57bfd9c2a599242b3f1e9d3
Simran Khosla <email.simrankhosla@gmail.com>
2015-12-27 18:43:33 +0100

    bootflix project completed with bonus

--- scripts/bootflixAjax.js
+++ scripts/bootflixAjax.js
@@ -7,7 +7,21 @@
  */
 app.getMovieById = function getMovieById(id) {
 
-  console.log("app.getMovieById() has been called. nothing happens. wait.. some tumbleweeds are tumbling by! an ID of '" + id + "' was entered.");
+  console.log("app.getMovieById() has been called. An ID of '" + id + "' was entered.");
+
+  console.log("app.getMovieById()" + id + "' was entered.");
+   $.ajax({
+     url: 'http://www.omdbapi.com/?i='+id+'&plot=full&r=json',
+     type: 'GET',
+   })
+   .done(function(data) {
+     console.log("success");
+     var movie = new app.MovieModel(data);
+     console.log(movie);
+
+     var view = new app.MovieView(movie);
+     });
+
 
   // request URL for omdb's id search
   // http://www.omdbapi.com/?i=tt0095016&plot=full&r=json
@@ -28,7 +42,8 @@ app.getMovieById = function getMovieById(id) {
  */
 app.getMovieByTitle = function getMovieByTitle(title) {
 
-  console.log("app.getMovieByTitle() has been called. the form stares at you blankly. wait, what? A title of '" + title + "' was entered");
+  console.log("app.getMovieByTitle() has been called.  A title of '" + title + "' was entered");
+
 
   // request URL for omdb's title search:
   //http://www.omdbapi.com/?t=Die+Hard&y=1988&plot=full&r=json
@@ -52,6 +67,14 @@ app.getMovieByTitle = function getMovieByTitle(title) {
  */
 app.MovieModel = function MovieModel(options) {
 
+  this.id = options.imdbID;
+  this.title = options.Title;
+  this.rating = options.imdbRating;
+  this.plot = options.Plot;
+  this.year = options.Year;
+  this.genre = options.Genre;
+  this.director = options.Director;
+  this.poster = options.Poster;
   // id, title, rating, director, plot, year, genre should all be in the `options` object
   // store all the information in the model
 
@@ -63,6 +86,8 @@ app.MovieModel = function MovieModel(options) {
  * @param options  - options object
  */
 app.MovieView = function MovieView(options) {
+
+  $( '#movie-listing').append("<div class='movie'><table><tr><td><img src='"+options.poster+"'alt='"+options.title+"'></td><td><h3>"+options.title+"</h3><p><strong>Released:</strong>"+options.year+"<br><strong>Directed By:</strong>"+options.director+"<br><em>"+options.genre+"</em></p><p>"+options.plot+"</p></td></tr></table></div>");
 
   // options should contain the `model` for which the view is using
 
